17a3c2f4b8e5d769b7e5085f6caae04ccc3f1eff
Aleksandr Vyatkin <aleksandr.vyatkin@rtmis.ru>
2021-12-22 23:27:32 +0500

    Initial Commit

---
+++ ML Car Detector/ML_Car_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_Car_DetectorApp.swift
+//  ML Car Detector
+//
+//  Created by Александр Вяткин on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ML_Car_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
